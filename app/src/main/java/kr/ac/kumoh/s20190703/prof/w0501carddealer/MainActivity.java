11fc0d2ed1686c7acad2f61c475ddc8a093506ac
엄태욱 <utsi09@naver.com>
2022-10-12 22:48:16 +0900

    revise

---
+++ app/src/main/java/kr/ac/kumoh/s20190703/prof/w0501carddealer/MainActivity.java
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.s20190703.prof.w0501carddealer;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
--- app/src/main/java/kr/ac/kumoh/s20190703/prof/w0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190703/prof/w0501carddealer/MainActivity.kt
@@ -1,18 +1,76 @@
-package kr.ac.kumoh.s20190703.prof.w0501carddealer
+package kr.ac.kumoh.prof.w0501precarddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kr.ac.kumoh.s20190703.prof.w0501carddealer.databinding.ActivityMainBinding
+import android.util.Log
+import kr.ac.kumoh.prof.w0501precarddealer.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding //레이아웃 XML 파일의 이름을 파스칼 케이스로 쓰고 뒤에 Binding 붙이면 클래스 이름이 됨
+    private lateinit var binding: ActivityMainBinding  //1
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater) //2
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root) //3
 
-        binding = ActivityMainBinding.inflate(layoutInflater)//기존 12 라인의 setContentView()를 13~14라인으로 변경
-        setContentView(binding.root)
+        //binding.card1.setImageResource(R.drawable.c_2_of_hearts)
+//        val c = Random.nextInt(52)
+//        Log.i("Test", "$c : ${getCardName(c)}")
+//
+//        val res = resources.getIdentifier(
+//            getCardName(c),
+//            "drawable",
+//            packageName
+//        )
+//
+//        binding.card1.setImageResource(res)
+        binding.btnDeal.setOnClickListener {
+            val c = IntArray(5)
+            val res = IntArray(5)
 
-        binding.card1.setImageResource(R.drawable.c_2_of_hearts) //이미지뷰에 리소스 지정하는 소스 추가
+            //for (i in 0..4)
+            //for (i in 0 until 5)
+            //for (i in 0 until c.size)
+            for (i in c.indices) {
+                c[i] = Random.nextInt(52)
+
+                Log.i("Test", "${c[i]} : " +
+                        "${getCardName(c[i])}")
+
+                res[i] = resources.getIdentifier(
+                    getCardName(c[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+
+            //card1.setImageResource(R.drawable.c_2_of_hearts)
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])
+        }
+    }
+
+    private fun getCardName(c: Int) : String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+
+        val number = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
     }
 }
